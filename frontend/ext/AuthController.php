1cb6b7a458edfad5f746495b67bb86ca68b76173
Yepifanov Serhii <serhiiyepifanov@doubledmarketing.com>
2022-12-27 13:02:28 +0200

    AuthController. init

--- frontend/ext/AuthController.php
+++ frontend/ext/AuthController.php
@@ -1,9 +1,72 @@
 <?php
 namespace frontend\ext;
 
+use yii\base\InvalidRouteException;
+use yii\base\Module;
 use yii\web\Controller;
+use Yii;
 
 class AuthController extends Controller
 {
-    public $layout = 'chat';
+    protected ?array $userArr = [];
+
+    protected function getCurrentUser()
+    {
+        if (empty($this->userArr)) {
+            $this->userArr = Yii::$app->user->identity;
+        }
+
+        return $this->userArr;
+    }
+
+    public function runAction($id, $params = [])
+    {
+        $action = $this->createAction($id);
+        if ($action === null) {
+            throw new InvalidRouteException('Unable to resolve the request: ' . $this->getUniqueId() . '/' . $id);
+        }
+
+        Yii::debug('Route to run: ' . $action->getUniqueId(), __METHOD__);
+
+        if (Yii::$app->requestedAction === null) {
+            Yii::$app->requestedAction = $action;
+        }
+
+        $oldAction = $this->action;
+        $this->action = $action;
+
+        $modules = [];
+        $runAction = true;
+
+        // call beforeAction on modules
+        foreach ($this->getModules() as $module) {
+            if ($module->beforeAction($action)) {
+                array_unshift($modules, $module);
+            } else {
+                $runAction = false;
+                break;
+            }
+        }
+
+        $result = null;
+
+        if ($runAction && $this->beforeAction($action)) {
+            // run the action
+            $result = $action->runWithParams($params);
+            $result = $this->afterAction($action, $result);
+
+            // call afterAction on modules
+            foreach ($modules as $module) {
+                /* @var $module Module */
+                $result = $module->afterAction($action, $result);
+            }
+        }
+
+        if ($oldAction !== null) {
+            $this->action = $oldAction;
+        }
+
+        return $result;
+    }
+
 }
