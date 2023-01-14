a34edc5c9fd3fcd8c9d0301c64c82bc6ed8e02f5
Yepifanov Serhii <serhiiyepifanov@doubledmarketing.com>
2023-01-14 23:24:43 +0200

    Fixed 2nd layout.

--- frontend/views/layouts/_chat_default.php
+++ frontend/views/layouts/_chat_default.php
@@ -3,14 +3,28 @@
 ?>
 
 <?php $this->beginContent('@frontend/views/layouts/chat.php'); ?>
-
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-offset-2 col-md-8">
-                <?= \frontend\widgets\CookieAlert::widget(); ?>
-            </div>
-        </div>
-
-        <?php echo $content ?>
+    <div class="nbeTrHeader">
+        <?php echo $this->render('_top_menu'); ?>
     </div>
+
+    <div class="wrap">
+        <div class="container-fluid">
+
+                <div class="row">
+                    <div class="col-md-offset-2 col-md-8">
+                        <?= \frontend\widgets\CookieAlert::widget(); ?>
+                    </div>
+                </div>
+
+                <?php echo $content ?>
+
+        </div>
+    </div>
+
+    <footer class="nbeFooter">
+        <div class="container-fluid">
+            <p class="pull-left">Chat created by <strong>Yepifanov Serhii</strong></p>
+            <p class="pull-right">2022 - <?php echo date('Y'); ?></p>
+        </div>
+    </footer>
 <?php $this->endContent(); ?>
