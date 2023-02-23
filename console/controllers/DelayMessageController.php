e984d4d04f0e25f9cfb1f17f543b9ccc459174ee
Yepifanov Serhii <serhiiyepifanov@doubledmarketing.com>
2023-02-24 00:15:31 +0200

    DelayMessage. create-test action. part3

--- console/controllers/DelayMessageController.php
+++ console/controllers/DelayMessageController.php
@@ -24,11 +24,56 @@ use yii\console\Controller;
  */
 class DelayMessageController extends Controller
 {
+    // TODO:
+    const MAX_CYCLE_TIME = 60;
+//    const MAX_CYCLE_TIME = 5;
+
     public function actionIndex()
     {
+        set_time_limit(120);
+
+        $startTime = $this->getTimeStampWithStartAt0(0);
+        $endTime = $startTime + self::MAX_CYCLE_TIME;
+
+        $insertTime = $startTime;
+        echo PHP_EOL, date('Y-m-d H:i:s', $insertTime), PHP_EOL;
+
+        // до тех пор, пока скрипт не пройдет по всем секундам (от 0 до 59 включительно) от тек. минуты
+        while ($insertTime < $endTime) {
+            $currentTime = time();
+
+
+            // вставка данных
+
+            // если данных нет, то ожидаем
+
+
+            echo PHP_EOL, 'ts=', date('Y-m-d H:i:s', $insertTime);
+            echo PHP_EOL, 'cd=', date('Y-m-d H:i:s', $currentTime);
+            // если "текущая секунда" больше "секунды вставки", то увеличиваем последнюю на 1
+            if ($currentTime > $insertTime) {
+                $insertTime++;
+            } else {
+                // ожидаем секунду
+                usleep(1000000);
+            }
+        }
+
+        $startTime = $endTime;
+        $endTime = $endTime + 30;
+        $dataExists = true;
+        while ($dataExists) {
+            // читаем данные
+
+            // если данных нет - выходим
+            $dataExists = false;
+        }
+
         return false;
     }
 
+    // создание тестовых записей, с "отрывом в секундах",
+    // если значение 60, то будет вставка с timestamp "следующей минуты"
     public function actionCreateTest($delayInSeconds = 60, $insertCount = 2000)
     {
         echo "action CreateTest started", PHP_EOL;
