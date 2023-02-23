7bb4125b7e7a09bede262ea73067437da67aacfe
Yepifanov Serhii <serhiiyepifanov@doubledmarketing.com>
2023-02-23 23:25:40 +0200

    DelayMessage. create-test action. part2

--- console/controllers/DelayMessageController.php
+++ console/controllers/DelayMessageController.php
@@ -2,6 +2,7 @@
 namespace console\controllers;
 
 use common\models\redis\DelayMsgSortedSetStorage;
+use DateTime;
 use yii\console\Controller;
 
 /**
@@ -28,30 +29,55 @@ class DelayMessageController extends Controller
         return false;
     }
 
-    public function actionCreateTest($delayInSeconds = 60)
+    public function actionCreateTest($delayInSeconds = 60, $insertCount = 2000)
     {
         echo "action CreateTest started", PHP_EOL;
-        $startAt = time();
 
-        for ($i = 0; $i < 10; $i++) {
-            DelayMsgSortedSetStorage::getInstance()->addTo(
-                $startAt,
-                '[' . date('Y-m-d-H-i-s') . '] message-' . rand(1, 10000));
+        $startAt = $this->getTimeStampWithStartAt0($delayInSeconds);
+        $this->print_time();
+
+        $resInserted = 0;
+        for ($i = 0; $i < 60; $i++) {
+            $this->print_time();
+
+            for ($j = 0; $j < $insertCount; $j++) {
+                $resInserted += (int) DelayMsgSortedSetStorage::getInstance()
+                    ->addTo(
+                        $startAt,
+                        '[' . date('Y-m-d-H-i-s') . '] message-' . rand(1, 10000000)
+                    );
+            }
+            $this->print_time();
         }
 
-//        $res = DelayMsgSortedSetStorage::getInstance()->getData(0, 100000000000, true);
-//        print_r2($res);
-        $res = DelayMsgSortedSetStorage::getInstance()->getData(1677103277, 1677103277, true, true);
-        print_r2($res);
-
-        $resR = DelayMsgSortedSetStorage::getInstance()->removeByScore(1677103277, 1677103277);
-        var_dump($resR); echo PHP_EOL;
-
-        $res = DelayMsgSortedSetStorage::getInstance()->getData(0, 100000000000, true);
-        print_r2($res);
-
+        echo PHP_EOL, 'inserted=[', $resInserted, ']', PHP_EOL;
 
         echo PHP_EOL, "action CreateTest ended", PHP_EOL;
         return false;
+    }
+
+    protected function print_time()
+    {
+        $now = DateTime::createFromFormat('U.u', microtime(true));
+        echo PHP_EOL, $now->format("m-d-Y H:i:s.u");
+    }
+
+    protected function getTimeStampWithStartAt0($delayInSeconds)
+    {
+        $startAt = time() + (int) $delayInSeconds;
+
+        return $startAt - ($startAt % 60);
+    }
+
+    // удаляет только лишь указанные в score значения
+    protected function removeCertain(int $certainScore)
+    {
+        echo PHP_EOL, 'remove score = ', $certainScore, PHP_EOL;
+        $res = DelayMsgSortedSetStorage::getInstance()->getData($certainScore, $certainScore, true, true);
+        print_r2($res, 'Elements for remove');
+        $resR = DelayMsgSortedSetStorage::getInstance()->removeByScore($certainScore, $certainScore);
+        var_dump($resR); echo PHP_EOL;
+        $res = DelayMsgSortedSetStorage::getInstance()->getData(0, 100000000000, true);
+        print_r2($res, 'least elements');
     }
 }
