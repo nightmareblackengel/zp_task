5de28c545b48ea32cf5aea52e24f04ee31d22cad
Yepifanov Serhii <serhiiyepifanov@doubledmarketing.com>
2023-03-10 15:51:26 +0200

    RemoveMessage controller. Next Part.

--- console/controllers/MsgController.php
+++ console/controllers/MsgController.php
@@ -12,7 +12,12 @@ class MsgController extends ConsoleController
     const MAX_DAY_COUNT = 30;
     const MAX_MESSAGE_COUNT = 5000;
 
+    const RECIEVE_QUEUE_MSG_COUNT = 1000;
+
     // docker exec -it mphp /var/www/html/ztt.loc/yii msg/delete-by-settings
+
+    // TODO;
+    // запуск каждую минуту, но нельзя запустить, больше 1го экземпляра
 
     // поддержка истории до [1 / 7 / 30 дней | 500 / 1000 / 5000 сообщений] в зависимости от настройки на пользователе / канале.
     // Внимание! Если 2 пользователя ведут диалог и у одного из них настройка 30 дней, а у второго 7 дней - история хранится по максимальному сроку / количеству сообщений.
@@ -21,7 +26,7 @@ class MsgController extends ConsoleController
      *  запуск раз в час
      * выполняет удаление сообщений согласно настройкам пользователя.
      * если у пользователя нет настроек (или у разных пользователей указаны дни и сообщения)
-     * то сообщения будут удаляться по одному из максимальных условий: 5тыс сообщений или больше 30ти дней
+     * то сообщения будут удаляться по условию: 5тыс сообщений
      */
     public function actionDeleteBySettings()
     {
@@ -34,7 +39,7 @@ class MsgController extends ConsoleController
 
         $offset = 0;
         $limit = 0;
-        $ids = [3, 4, 25, 27, 30];
+        $ids = [3, 4, 25, 27, 29, 30];
 
         // для каждого чата
         $chats = ChatModel::getInstance()->getList([
@@ -52,43 +57,35 @@ class MsgController extends ConsoleController
             list($maxMessages, $maxDays) = $this->getChatUserSettingParams($chat['id']);
 
             echo PHP_EOL;
-            var_dump($maxMessages);
-            echo PHP_EOL;
-            var_dump($maxDays);
-            echo PHP_EOL;
+            var_dump($maxMessages, $maxDays);
 
             if (!empty($maxDays)) {
-                $removeCount = $this->removeMessagesByDay($chat['id'], $maxDays);
+                $removeCount = $this->removeMessagesByDayCount($chat['id'], $maxDays);
             } else {
-                $removeCount = $this->removeMessagesByCount($chat['id'], $maxMessages);
+                $removeCount = $this->removeMessagesByMsgCount($chat['id'], $maxMessages);
             }
             echo 'Для чата id=[', $chat['id'], '] было удалено ' . $removeCount . ' сообщений.', PHP_EOL;
-
-
-            exit();
         }
-
-
-
-
-        // получить настройки всех пользователей
-        // если у них разные настройки,
-        // то для каждой настройки получить максимальное значение и затем делать проверки
-
         echo "Метод успешно выполнен", PHP_EOL;
         return '';
     }
 
-
-    protected function removeMessagesByDay(int $chatId): int
+    protected function removeMessagesByDayCount(int $chatId, int $allowedDayCount): int
     {
+        $msgCountForRemove = $this->getChatExpiredByDayMsgCount($chatId, $allowedDayCount);
+        if (empty($msgCountForRemove)) {
+            return 0;
+        }
 
-        echo PHP_EOL, "Remove By Days Count", PHP_EOL;
+        $remRes = ChatMessageQueueStorage::getInstance()->removeItemCount($chatId, $msgCountForRemove);
+        if (empty($remRes)) {
+            return 0;
+        }
 
-        return 0;
+        return $msgCountForRemove;
     }
 
-    protected function removeMessagesByCount(int $chatId, int $allowedMsgCount): int
+    protected function removeMessagesByMsgCount(int $chatId, int $allowedMsgCount): int
     {
         if (empty($allowedMsgCount) || empty($chatId) || $allowedMsgCount < 1) {
             return 0;
@@ -116,5 +113,48 @@ class MsgController extends ConsoleController
         $settings = UserSettingsModel::getInstance()->getChatUserSettings($chatId);
 
         return UserSettingsHelper::prepareSettings($settings);
+    }
+
+    /**
+     * Метод для указанного чата получает все собщения (по пачкам),
+     * и сравнивает даты сообщений
+     * все сообщения которые не подходят по условию - будут посчитаны,
+     * и вовзращены как результат
+     * @param int $chatId
+     * @param int $allowedDayCount
+     * @return int
+     */
+    protected function getChatExpiredByDayMsgCount(int $chatId, int $allowedDayCount): int
+    {
+        if (empty($allowedDayCount) || $allowedDayCount < 1) {
+            return 0;
+        }
+        $lastTime = time() - $allowedDayCount * 24*60*60;
+        $receiveMsgIndex = 0;
+        $running = true;
+
+        while ($running) {
+            $messages = ChatMessageQueueStorage::getInstance()->getList($chatId, $receiveMsgIndex, $receiveMsgIndex + self::RECIEVE_QUEUE_MSG_COUNT - 1);
+            if (empty($messages)) {
+                $running = false;
+                continue;
+            }
+
+            $runSubCycle = true;
+            $i = 0;
+            while ($i < count($messages) && $runSubCycle) {
+                if (!empty($messages[$i]->d) && $messages[$i]->d > 1 && $lastTime <= $messages[$i]->d) {
+                    $runSubCycle = false;
+                    continue;
+                }
+                $i++;
+            }
+            $receiveMsgIndex += $i;
+            if (!$runSubCycle) {
+                $running = false;
+            }
+        }
+
+        return $receiveMsgIndex;
     }
 }
